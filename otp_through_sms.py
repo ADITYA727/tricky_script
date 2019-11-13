9700fc9a22a84ed2ff36c7d0f21014a7dc4b8dd0
ADITYA727 <adityashukla727@gmail.com>
2019-11-13 15:09:48 +0530

    sent sms

---
+++ otp_through_sms.py
@@ -0,0 +1,36 @@
+# import library 
+import math, random
+# importing twilio 
+from twilio.rest import Client
+
+# function to generate OTP 
+def generateOTP():
+        string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        OTP = "Wellcome to freebazar.com, and your otp is "
+        length = len(string)
+        for i in range(6):
+                OTP += string[math.floor(random.random() * length)]
+        OTP = OTP + " Thank you for connecting us..\n hope all is well"                
+        account_sid = 'ACab7dd9e6fb0cadfaf3286e5ceec09aec'
+        auth_token = 'f033559efa1a93f0177a3113edabc6eb'
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(body=OTP,from_='+12568264574',to='+917355177189')
+        print(message.sid)
+        return OTP
+
+
+# Driver code 
+if __name__ == "__main__" : 
+	
+	print("OTP of length 6:", generateOTP())
+
+
+
+ 
+
+
+ 
+
+ 
+
+	
