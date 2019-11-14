7a982a61e9dc8f9318e9ccd5bf73537b87da6296
ADITYA727 <adityashukla727@gmail.com>
2019-11-14 16:38:57 +0530

    update send otp module

--- otp_through_sms.py
+++ otp_through_sms.py
@@ -5,6 +5,8 @@ from twilio.rest import Client
 
 # function to generate OTP 
 def generateOTP():
+        num = '+91'
+        num = num + input("enter number ..")
         string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
         OTP = "Wellcome to freebazar.com, and your otp is "
         length = len(string)
@@ -14,15 +16,14 @@ def generateOTP():
         account_sid = 'ACab7dd9e6fb0cadfaf3286e5ceec09aec'
         auth_token = 'f033559efa1a93f0177a3113edabc6eb'
         client = Client(account_sid, auth_token)
-        message = client.messages.create(body=OTP,from_='+12568264574',to='+917355177189')
+        message = client.messages.create(body=OTP,from_='+12568264574',to=num)
         print(message.sid)
         return OTP
 
 
 # Driver code 
-if __name__ == "__main__" : 
-	
-	print("OTP of length 6:", generateOTP())
+if __name__ == "__main__" :
+        print("OTP of length 6:", generateOTP())
 
 
 
